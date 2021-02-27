76d98eccf06e5c1c4cc0dd394033d8269eb9dd56
Mark Johnson <markandersjohnson@gmail.com>
2021-02-27 12:47:12 -0600

    added youtube component

--- src/js/cms.js
+++ src/js/cms.js
@@ -16,4 +16,27 @@ CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
 CMS.registerPreviewTemplate("values", ValuesPreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
+
+CMS.registerEditorComponent({
+    id: "youtube",
+    label: "Youtube",
+    fields: [{
+        name: "id",
+        label: "Youtube Video ID",
+        widget: "string"
+    }],
+    pattern: /{{< youtube ([a-zA-Z0-9]+) >}}/,
+    fromBlock: function(match) {
+        return {
+            id: match[1],
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< youtube ${obj.id} >}}`;
+    },
+    toPreview: function(obj) {
+        return `<img src="http://img.youtube.com/vi/${obj.id}/maxresdefault.jpg" alt="Youtube Video"/>`;
+    },
+});
+
 CMS.init();
